c98323b67f35734f7e746124d386ed07f6144bd2
Soulayman-A <soulayman.arslane@ynov.com>
2025-10-20 16:14:13 +0200

    add endpoint  /login x /register

--- gestion-utilisateur-back/src/test/java/com/example/security/SecurityApplicationTests.java
+++ gestion-utilisateur-back/src/test/java/com/example/security/SecurityApplicationTests.java
@@ -1,10 +1,10 @@
-package com.gestion.gestionUtilisateur;
+package com.example.security;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GestionUtilisateurApplicationTests {
+class SecurityApplicationTests {
 
 	@Test
 	void contextLoads() {
