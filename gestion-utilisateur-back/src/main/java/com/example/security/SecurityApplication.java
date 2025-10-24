a48b264f83f0b424ec83d5fd4c3a1b34899450ad
Soulayman-A <soulayman.arslane@ynov.com>
2025-10-24 11:08:26 +0200

    update .env  + add schema.sql

--- gestion-utilisateur-back/src/main/java/com/example/security/SecurityApplication.java
+++ gestion-utilisateur-back/src/main/java/com/example/security/SecurityApplication.java
@@ -1,11 +1,23 @@
 package com.example.security;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SecurityApplication {
+
     public static void main(String[] args) {
-        SpringApplication.run(SecurityApplication.class, args);
+        SpringApplication app = new SpringApplication(SecurityApplication.class);
+
+        // 🔹 Charge .env AVANT Spring Boot
+        app.addInitializers((ApplicationContextInitializer<ConfigurableApplicationContext>) ctx -> {
+            Dotenv dotenv = Dotenv.load();
+            dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+        });
+
+        app.run(args);
     }
 }
